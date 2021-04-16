349413066b85b287afd9e9baf23e83a1d1378edc
Nikolay Bukarinov <bukarinov@gmail.com>
2021-04-16 11:25:35 +0300

    Add two neggative tests for Item class. Add getters for Item's properties. Extend the positive test for checking properties values.

--- src/Catalog/Domain/Item.php
+++ src/Catalog/Domain/Item.php
@@ -8,4 +8,14 @@ class Item
         private string $sku,
         private float $price,
     ) {}
+
+    public function getSku(): string
+    {
+        return $this->sku;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
 }
--- tests/Unit/Catalog/Domain/ItemTest.php
+++ tests/Unit/Catalog/Domain/ItemTest.php
@@ -7,14 +7,37 @@ use PHPUnit\Framework\TestCase;
 
 class ItemTest extends TestCase
 {
-    /** @test */
-    public function construct_GivenSkuAndPrice_ObjectIsCreatedSuccessfully()
+    public function test_GivenSkuAndPrice_ObjectIsCreatedSuccessfully()
     {
-        $item = new Item(sku: 'A', price: 45);
+        $item = new Item(sku: 'A', price: 42);
 
         $this->assertInstanceOf(
             Item::class,
             $item
         );
+
+        $this->assertEquals(
+            'A',
+            $item->getSku()
+        );
+
+        $this->assertEquals(
+            42,
+            $item->getPrice()
+        );
+    }
+
+    public function test_GivenSkuOnly_ThrownException()
+    {
+        $this->expectException(\ArgumentCountError::class);
+
+        new Item(sku: 'A');
+    }
+
+    public function test_GivenPriceOnly_ThrownException()
+    {
+        $this->expectException(\ArgumentCountError::class);
+
+        new Item(price: 42);
     }
 }
